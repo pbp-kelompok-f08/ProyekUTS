4c1e6037832cdf83a91755a6770408f258c0cec1
elgasing23 <abinaufal901@gmail.com>
2025-10-08 17:35:18 +0700

    account

---
+++ accounts/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+    path('login/', login_page, name='login'),
+    path('register/', register_page, name='register'),
+    path('dashboard/', dashboard, name='dashboard'),
+
+    path('ajax/login/', login_ajax, name='login_ajax'),
+    path('ajax/register/', register_ajax, name='register_ajax'),
+    path('ajax/logout/', logout_ajax, name='logout_ajax'),
+    path('ajax/delete-user/<int:user_id>/', admin_delete_user, name='admin_delete_user'),
+]
