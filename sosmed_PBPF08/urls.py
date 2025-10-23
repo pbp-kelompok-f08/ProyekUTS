6a16d5125c0992501184d9bdbd88a9e5221e6f6e
Fitto Fadhelli Voltanie Ariyana <voltaniefitto@gmail.com>
2025-10-23 12:55:17 +0700

    Change booking success path to booking venue
    biar lebih mudah nge baca

--- sosmed_PBPF08/urls.py
+++ sosmed_PBPF08/urls.py
@@ -26,6 +26,6 @@ urlpatterns = [
     path('', include('accounts.urls', namespace='accounts')),
     path('matches/', include('matches.urls', namespace='matches')),
     path('threads/', include('threads.urls', namespace='threads')),
-    path('booking-success/', include('booking_venue.urls', namespace='booking_venue')),
+    path('booking-venue/', include('booking_venue.urls', namespace='booking_venue')),
     path('', views.home, name="home"),
 ]
