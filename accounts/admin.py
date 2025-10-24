77f62690cab6f646ef42118faa8380a9218e8a56
elgasing23 <abinaufal901@gmail.com>
2025-10-24 19:22:14 +0700

    hubungain accoutn ke admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 
-# Register your models here.
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ("username", "email", "role", "is_staff", "last_activity")
+    list_filter = ("role", "is_staff", "is_active")
+    fieldsets = (
+        (None, {"fields": ("username", "password")}),
+        ("Personal Info", {"fields": ("first_name", "last_name", "email", "bio", "profile_picture", "favorite_sport", "skill_level")}),
+        ("Permissions", {"fields": ("role", "is_staff", "is_active", "groups", "user_permissions")}),
+        ("Important dates", {"fields": ("last_login", "last_activity", "date_joined")}),
+    )
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": ("username", "email", "password1", "password2", "role", "is_staff", "is_active"),
+        }),
+    )
+    search_fields = ("username", "email")
+    ordering = ("username",)
