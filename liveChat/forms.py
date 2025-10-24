64f9d14c8fed818e7dab6c75c4a781e54d90d0a5
elgasing23 <abinaufal901@gmail.com>
2025-10-24 17:11:30 +0700

    Fixing internal server error

--- liveChat/forms.py
+++ liveChat/forms.py
@@ -1,11 +1,18 @@
 from django import forms
 from .models import Group, Chat
-from typing import override
+
+try:
+    from typing import override  # Python 3.12+ (buat lokal kamu yang pakai 3.13)
+except ImportError:
+    def override(func):  # fallback buat Python 3.11 (PWS)
+        return func
+
 
 class GroupForm(forms.ModelForm):
     class Meta:
         model = Group
         fields = ["match", "name"]
+
 
 class ChatForm(forms.ModelForm):
     class Meta:
